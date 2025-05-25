704ab79d51044dc5d21298deb746fbbc4de8b061
Sven Patrick Meier <sven.patrick.meier@gmail.com>
2025-05-25 09:55:47 +0200

    feat: add product views to application

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Category, Product
 
 # Create your views here.
+def product_list(request, category_slug=None):
+    products = Product.objects.all().order_by('-created_at')
+    categories = Category.objects.all().order_by('name')
+    
+    if category_slug != None:
+        category_page = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=category_page)
+
+    context = {
+        'products': products,
+        'categories': categories
+    }
+
+    return render(request, 'products.html', context)
+
+def product_detail(request, category_slug, product_id):
+    product = Product.objects.get(category__slug=category_slug, id=product_id)
+    products = Product.objects.all().order_by('-created_at')
+    
+    context = {
+        'product': product,
+        'products': products[0:5],
+    }
+    
+    return render(request, 'product.html', context)
