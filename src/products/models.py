da9b63222dc8ad5d152ea26a2f3080c8a04317b0
Sven Patrick Meier <sven.patrick.meier@gmail.com>
2025-05-27 19:51:22 +0200

    test: add model tests for btw project

--- src/products/models.py
+++ src/products/models.py
@@ -1,10 +1,13 @@
+from decimal import Decimal
 from django.db import models
+from django.core.validators import MinValueValidator
+
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=50, null=True)
+    name = models.CharField(max_length=50, unique=True, null=False, blank=False)
     description = models.TextField(max_length=200, null=True, blank=True)
-    slug = models.SlugField(max_length=50, unique=True, null=True)
+    slug = models.SlugField(max_length=50, unique=True, null=False, blank=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -22,8 +25,14 @@ class Product(models.Model):
     category = models.ForeignKey(Category, null=True, on_delete=models.DO_NOTHING)
     description = models.TextField(max_length=250, null=True, blank=True)
     image = models.ImageField(upload_to="products/", null=True, blank=True)
-    name = models.CharField(max_length=80)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    name = models.CharField(max_length=80, blank=False, null=False)
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2, 
+        validators=[
+            MinValueValidator(Decimal('0.00'))
+        ]
+    )
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
