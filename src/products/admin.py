b97b8bc419b35928bb16612152ac0398090f0fbf
Sven Patrick Meier <sven.patrick.meier@gmail.com>
2025-08-20 11:34:40 +0200

    docs: add prerequisites notes to readme
    closes #9

--- src/products/admin.py
+++ src/products/admin.py
@@ -7,6 +7,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_display=('name','price', 'category', 'created_at', 'updated_at')
     search_fields=('name','category')
 
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields={'slug':('name',)}
